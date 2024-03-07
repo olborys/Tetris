b3d5ce698aee7927987a3d47cf8d892bb737d210
Oleh Borys <olv.borys@gmail.com>
2024-03-07 11:29:12 +0100

    add game

--- tetris.js
+++ tetris.js
@@ -1,36 +1,75 @@
-import {
-  PLAYFIELD_COLUMNS,
-  PLAYFIELD_ROWS,
-  TETROMINOES,
-  TETROMINO_NAMES,
-  getRandomElement,
-} from "./utilities.js";
+export const PLAYFIELD_COLUMNS = 10;
+export const PLAYFIELD_ROWS = 20;
+export const TETROMINO_NAMES = ["I", "J", "L", "O", "S", "Z", "T"];
+export const TETROMINOES = {
+  I: [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+  J: [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  L: [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  T: [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+};
 
-export class Tetris {
-  constructor() {
-    this.playfield;
-    this.tetromino;
-    this.init();
-  }
-  init() {
-    this.generatePlayField();
-    this.generateTetromino();
-  }
-  generatePlayField() {
-    this.playfield = new Array(PLAYFIELD_ROWS)
-      .fill()
-      .map(() => new Array(PLAYFIELD_COLUMNS).fill(0));
-  }
-  generateTetromino() {
-    const name = getRandomElement(TETROMINO_NAMES);
-    const matrix = TETROMINOES[name];
-    const column = PLAYFIELD_COLUMNS / 2 - Math.floor(matrix.length / 2);
-    const row = 3;
-    this.tetromino = {
-      name,
-      matrix,
-      row,
-      column,
-    };
-  }
+export function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
+
+export function rotateMatrix(matrix) {
+  const N = matrix.length;
+  const rotatedMatrix = [];
+  for (let i = 0; i < N; i++) {
+    rotatedMatrix[i] = [];
+    for (let j = 0; j < N; j++) {
+      rotatedMatrix[i][j] = matrix[N - j - 1][i];
+    }
+  }
+  return rotatedMatrix;
+}
+
+export function convertPositionToIndex(row, column) {
+  return row * PLAYFIELD_COLUMNS + column;
+}
+
+export const SAD = [
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+  [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+];
--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 export const PLAYFIELD_COLUMNS = 10;
 export const PLAYFIELD_ROWS = 20;
-export const TETROMINO_NAMES = ["I", "J", "L", "O", "Z", "T"];
+export const TETROMINO_NAMES = ["I", "J", "L", "O", "S", "Z", "T"];
 export const TETROMINOES = {
   I: [
     [0, 0, 0, 0],
@@ -43,3 +43,33 @@ export function getRandomElement(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 }
+
+export function rotateMatrix(matrix) {
+  const N = matrix.length;
+  const rotatedMatrix = [];
+  for (let i = 0; i < N; i++) {
+    rotatedMatrix[i] = [];
+    for (let j = 0; j < N; j++) {
+      rotatedMatrix[i][j] = matrix[N - j - 1][i];
+    }
+  }
+  return rotatedMatrix;
+}
+
+export function convertPositionToIndex(row, column) {
+  return row * PLAYFIELD_COLUMNS + column;
+}
+
+export const SAD = [
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+  [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+];
